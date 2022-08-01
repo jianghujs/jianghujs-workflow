d964dafc8e869e9bd392c69b86d2ed5565d6d968
case <7795958+lipangpang251@user.noreply.gitee.com>
2022-08-01 22:30:38 +0800

    ui改版提交

--- config/config.default.js
+++ config/config.default.js
@@ -21,7 +21,7 @@ module.exports = appInfo => {
     appLogo: `${appId}/public/img/logo.png`,
     appType: 'single',
     appDirectoryLink: '/',
-    indexPage: `/${appId}/page/manual`,
+    indexPage: `/${appId}/page/workflowManagement`,
     loginPage: `/${appId}/page/login`,
     helpPage: `/${appId}/page/help`,
     uploadDir,
